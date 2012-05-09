590a2f77b870d738ba61d7adec2ab55bd51ffc37
Damien Del Russo <ddelruss@mac.com>
2012-05-09 09:59:03 -0400

    Fixed bug based on difference in frame size between simulator and actual device.

--- UIClearToolbar/UIClearToolbar.m
+++ UIClearToolbar/UIClearToolbar.m
@@ -13,7 +13,8 @@
 - (void)layoutSubviews {
     // super has already laid out the subviews before this call is made.
     [self.subviews enumerateObjectsUsingBlock:^(UIView* obj, NSUInteger idx, BOOL *stop) {
-        if (CGSizeEqualToSize(self.frame.size, obj.frame.size)) {
+        if (CGSizeEqualToSize(self.frame.size, obj.frame.size) ||
+            self.frame.size.width <= obj.frame.size.width) {  // on device, the background is BIGGER than the toolbar.
             [obj removeFromSuperview];
             *stop = YES;
         }
